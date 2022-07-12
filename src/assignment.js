4109e3317da40ca329f669b490f7a71f9a9cd680
Ayantokun Ayantade Blog <105325483+Nathan-td@users.noreply.github.com>
2022-07-12 22:36:38 +0000

    attempted-challenge 1 & 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+  for(i = 0; i < arrayOfNumbers.length; i++){
+    sum += arrayOfNumbers[i];
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let ArrayOfevenNumbers = []
+  for(i = 0; i < arrayOfNumbers.length; i++){
+    let evenNumber = arrayOfNumbers[i] % 2 === 0;
+
+    if(evenNumber){
+      ArrayOfevenNumbers.push(evenNumber)
+    }
+  }
+    return ArrayOfevenNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
